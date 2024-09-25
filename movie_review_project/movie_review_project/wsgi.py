3f61574842295a869e23207680bab034d87c4142
Janassalem <janamohamed248@gmail.com>
2024-09-25 21:47:14 +0300

    x

--- movie_review_project/movie_review_project/wsgi.py
+++ movie_review_project/movie_review_project/wsgi.py
@@ -7,35 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-# import os
-
-
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_review_project.settings')
-
-# application = get_wsgi_application()
-
-
-
-
-
 import os
-import sys
+
 from django.core.wsgi import get_wsgi_application
 
-# Add your project directory to the sys.path
-project_home = '/home/janassalem/Movie_Recommendations'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_review_project.settings')
 
-# Set environment variable for Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Movie_Recommendations.settings'
-
-# Activate virtual environment
-activate_this = '/home/janassalem/Movie_Recommendations/venv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
-# Import Django's WSGI handler to handle requests
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
