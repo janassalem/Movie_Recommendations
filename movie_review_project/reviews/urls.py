5c904cfbcab2d663aec76633e97026181b44ec45
Janassalem <janamohamed248@gmail.com>
2024-09-29 16:44:16 +0300

    c

--- movie_review_project/reviews/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ReviewListCreate, ReviewDetail, UserCreate, UserList
-
-urlpatterns = [
-    path('reviews/', ReviewListCreate.as_view(), name='review-list-create'),
-    path('reviews/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
-    path('users/', UserCreate.as_view(), name='user-create'),
-    path('users/list/', UserList.as_view(), name='user-list'),
-]
